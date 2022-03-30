30ea0edb390e8a73b84420fef6ebac67f8e4414d
Divyansh Bhatt <jagrandivyansh@gmail.com>
2022-03-30 13:20:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = new Date();
+  const year = date.getFullYear(); 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
       </header>
+      <footer>
+        Divyansh_bhatt
+        ©{year}
+      </footer>
     </div>
   );
 }
